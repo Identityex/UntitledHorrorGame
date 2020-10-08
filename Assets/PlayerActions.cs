b0f770eb77bd44ba7447ba5acb65aa648fea3f48
Travis Arsenault <travis@thirdstream.ca>
2020-10-08 14:45:06 -0400

    Import Assets and setup player movement

---
+++ Assets/PlayerActions.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+namespace Player.Actions
+{
+    public class PlayerActions : MonoBehaviour
+    {
+        private const string Horizontal = "Horizontal";
+        private const string Ground = "Ground";
+
+        private bool _onGround;
+        protected AudioSource _source;
+        protected BoxCollider2D bc;
+
+        protected int movementSpeed = 10;
+        protected Rigidbody2D player;
+        private int runningSpeed => movementSpeed * 2;
+
+        public void Start()
+        {
+            player = GetComponent<Rigidbody2D>();
+            _onGround = false;
+            bc = gameObject.AddComponent<BoxCollider2D>();
+            _source = GetComponent<AudioSource>();
+        }
+
+
+        public void Update()
+        {
+            var dt = Time.deltaTime;
+            var movement =
+                new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+            if (movement.magnitude > 1) //Normalizing movement
+                movement = movement / movement.magnitude;
+
+            transform.Translate(movement * dt * movementSpeed);
+
+            if (IsGrounded() && Input.GetButtonDown("Jump")) //rb is the player's rigidbody
+                player.velocity = new Vector3(player.velocity.x, movementSpeed);
+        }
+
+        public void OnCollisionEnter2D(Collision2D colInfo)
+        {
+            if (colInfo.gameObject.CompareTag(Ground))
+            {
+                _onGround = true;
+                _source.Play();
+            }
+        }
+
+        public void OnCollisionExit2D(Collision2D colInfo)
+        {
+            if (colInfo.gameObject.CompareTag(Ground)) _onGround = false;
+        }
+
+        public void OnCollisionStay2D(Collision2D colInfo)
+        {
+            if (colInfo.gameObject.CompareTag(Ground)) _onGround = true;
+        }
+
+        //...
+        public bool IsGrounded()
+        {
+            return _onGround;
+        }
+    }
+}
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,53 +1,11 @@
-﻿using UnityEngine;
+﻿using Player.Actions;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : PlayerActions
 {
-    private const string Horizontal = "Horizontal";
-    private const string Ground = "Ground";
-
-    private const int jumpSpeed = 10;
-    private const int moveSpeed = 10;
-    public Rigidbody2D player;
-    private bool _onGround;
+    private PlayerMovement()
+    {
+        movementSpeed = 5;
+    }
 
     // Start is called before the first frame update
-    private void Start()
-    {
-        player = GetComponent<Rigidbody2D>();
-    }
-
-    // Update is called once per frame
-    private void Update()
-    {
-        var moveHorizontal = Input.GetAxis(Horizontal);
-
-        if (Input.GetKeyDown(KeyCode.Space) && _onGround)
-        {
-            var jumpMovement = new Vector2(0.0f, 1.0f);
-            player.velocity = jumpMovement * jumpSpeed;
-        }
-
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            var movement = new Vector2(1.0f, 0.0f);
-            player.velocity = movement * moveSpeed;
-        }
-
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            var movement = new Vector2(-1.0f, 0.0f);
-            player.velocity = movement * moveSpeed;
-        }
-    }
-
-
-    public void OnCollisionEnter(Collision colInfo)
-    {
-        if (colInfo.gameObject.CompareTag("Ground")) _onGround = true;
-    }
-
-    private void OnCollisionExit(Collision collisionInfo)
-    {
-        if (collisionInfo.gameObject.CompareTag("Ground")) _onGround = false;
-    }
 }
